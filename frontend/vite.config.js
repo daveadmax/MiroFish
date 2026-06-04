670221cb6ec235a63d25a63a0e61d22ae9c00262
daveadmax <77241141+daveadmax@users.noreply.github.com>
2026-06-04 20:57:09 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -14,6 +13,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    allowedHosts: ['mirofish.followthegreensheep.de'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
